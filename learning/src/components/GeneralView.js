cbf6b4a8e0f74333d37c4daaef08270f3f712d17
sirMelka <melkabotale1@gmail.com>
2022-08-04 01:53:42 +0100

    feature: design of conversation zone without the input message area

--- learning/src/components/GeneralView.js
+++ learning/src/components/GeneralView.js
@@ -9,7 +9,35 @@ const GeneralView = () => {
                 <span className='more'><i className="fa-solid fa-ellipsis-vertical"></i></span>
             </div>
 
-            <div class="discussion"></div>
+            <div class="discussion">
+            <div>
+                <div className="sended-message">
+                d'ailleur c'est une bonne chose pas de concurent veut dire pas de marché ,je viens de faire une petite évalution de ce marché , j'ai identifier un segment auquel ni toi ni tes concurrent ont touchés pour l'instant.
+                   
+                </div></div>
+                <div>
+                <div className="received-message">
+                d'ailleur c'est une bonne chose pas de concurent veut dire pas de marché ,je viens de faire une petite évalution de ce marché , j'ai identifier un segment auquel ni toi ni tes concurrent ont touchés pour l'instant.
+                   
+                </div></div>
+                <div><div className="sended-message">
+                j'ai bien mon étude du marché t'inquiète
+                   
+                </div></div>
+                <div>
+                <div className="received-message">
+                peu importe, fais appel à moi quand même, tu verras, je vais assurer de ouf
+                </div></div>
+                <div>
+                <div className="received-message">
+               Je te donnes même ma cousine si tu veux
+                </div></div>
+                <div>
+                <div className="sended-message">
+               Non merci Bro, je suis en couple avec Réact
+                </div></div>
+                
+            </div>
         </div>
        
     );
