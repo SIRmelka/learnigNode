32077cdb42095c476112bb47e36d2792abb88393
sirMelka <melkabotale1@gmail.com>
2022-08-05 23:19:26 +0100

    feature: setting up learners and messages api

--- learning/src/components/GeneralView.js
+++ learning/src/components/GeneralView.js
@@ -1,47 +1,50 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Avatar from './Avatar';
 import Message from './Message';
 import SendBox from './SendBox';
 
 
 const GeneralView = () => {
-    
+
+
+    const [messages,setMessage] = useState([])
+
+    useEffect(
+        ()=>{
+            axios.get("http://localhost:1337/api/messages?filters[from][$in][1]=2&filters[from][$in][2]=1")
+        .   then((data)=>setMessage(data.data.data))
+        },[]
+    )
+
+    const sended = true
+
     return (
         <div className='banner-menu general-view'>
              <div className='top'>
-                <Avatar url="https://z-p3-scontent.ffih1-2.fna.fbcdn.net/v/t39.30808-6/291026997_5262330957189980_2751945388083315861_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH2IIuHdrcgrO3O1YzbjF50fcGym8sIaYp9wbKbywhpinPgEPAk_i08OJnPcly8xlAiNoMp-QpU_C0Xb3dPeMMw&_nc_ohc=Rj6M8f6Ge8UAX_EUlDX&_nc_pt=5&_nc_zt=23&_nc_ht=z-p3-scontent.ffih1-2.fna&oh=00_AT___YujUE7bXTEKJ-OqYbXAXlykzTkgPsR1pr_HvDo4LQ&oe=62EEB933"/>
-                <span className='active-user'>Almerinda ngalu</span>
+                <Avatar url="https://z-p3-scontent.ffih1-2.fna.fbcdn.net/v/t1.6435-9/100771823_1127645820951085_2857457444429234176_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFqoggKI62NrLab9p55nNhb6x_vrLa_LR_rH--str8tHyWCbqtb59gmzb4Dz9Biszn3HKhH1HOCLwz_om0q2v0q&_nc_ohc=5X4wa9oF37sAX9LBZ6P&_nc_pt=5&_nc_ht=z-p3-scontent.ffih1-2.fna&oh=00_AT953cX_pVcOOsCp7JyL0MTEoxYvrOWptGY8jqfsGBpqqg&oe=6314E4E3"/>
+                <span className='active-user'>Honey Tshikuz</span>
                 <span className='more'><i className="fa-solid fa-ellipsis-vertical"></i></span>
             </div>
 
             <div className="discussion">
 
-                <Message 
-                    key={1}
-                    message={"salut comment"} 
-                    type={"received-message"} 
-                    date={"7:32"}
-                />
-                  <Message 
-                    key={2}
-                    message={"bien bro"} 
-                    type={"sended-message"} 
-                    date={"7:45"}
-                />
-                <Message 
-                    key={3}
-                    message={"tu as finis avec les props?"} 
-                    type={"received-message"} 
-                    date={"7:46"}
-                />
-                 <Message 
-                    key={4}
-                    message={"non mais bientôt, je n'arrives toujours pas à faire la différence entre props et state"} 
-                    type={"sended-message"} 
-                    date={"7:50"}
-                />
-        
+                {
+                   messages.map((message)=>{
+                    return(
+                        <Message 
+                            key={message.attributes.id} 
+                            message={message.attributes.message}
+                            date={"08:23"}
+                            type={sended?"sended-message":"received-message"}
+                            />
+
+                    )
+                   })
+                   
+                }
+
+                
                 
            </div>
            <SendBox /> 
